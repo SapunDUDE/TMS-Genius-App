46bbe8d61dc49d31de744511920cec88ed1e4cbf
SapunDUDE <sapundude@mail.ru>
2023-05-11 00:57:12 +0300

    Initial commit

---
+++ src/main/java/com/genius/tms_c61_genius/TmsC61GeniusApplication.java
@@ -0,0 +1,13 @@
+package com.genius.tms_c61_genius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsC61GeniusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmsC61GeniusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genius/tms_c61_genius/TmsC61GeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genius.tms_c61_genius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsC61GeniusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
